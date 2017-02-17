31edc1c9e85c30b74632d9ac2d3f81ed532603ab
adamnew123456 <adamnew123456@gmail.com>
2017-02-17 13:26:09 -0500

    Add tests to check that <key> and escape sequences work

--- src/test/java/org/leaderkey/config/TestParser.java
+++ src/test/java/org/leaderkey/config/TestParser.java
@@ -75,4 +75,95 @@ public class TestParser {
             fail(err.toString());
         }
     }
+
+    @Test(timeout=5000)
+    public void testKeyNames() {
+        String[][] key_name_values = {
+            {"Space", " "},
+            {"Tilde", "~"},
+            {"Backtick", "`"},
+            {"Exclamation", "!"},
+            {"At", "@"},
+            {"Pound", "#"},
+            {"Dollar", "$"},
+            {"Percent", "%"},
+            {"Carat", "^"},
+            {"Ampersand", "&"},
+            {"Star", "*"},
+            {"LParen", "("},
+            {"RParen", ")"},
+            {"Dash", "-"},
+            {"Underscore", "_"},
+            {"Plus", "+"},
+            {"Equals", "="},
+            {"LBrace", "{"},
+            {"RBrace", "}"},
+            {"LBracket", "["},
+            {"RBracket", "]"},
+            {"Backslash", "\\"},
+            {"Pipe", "|"},
+            {"Colon", ":"},
+            {"Semicolon", ";"},
+            {"Quote", "'"},
+            {"DoubleQuote", "\""},
+            {"LAngle", "<"},
+            {"RAngle", ">"},
+            {"Question", "?"},
+            {"Slash", "/"}
+        };
+
+        for (int i = 0; i < key_name_values.length; i++) {
+            String key_name = key_name_values[i][0];
+            String key_chars = key_name_values[i][1];
+
+            Parser parser = parserFromString(
+                "<" + key_name + "> \"test\" `doit`\n");
+
+            try {
+                LeaderGroup actual = parser.parse();
+
+                LeaderGroup expected = new LeaderGroup();
+                LeaderAction action = new LeaderAction("doit", EMPTY_RUNNER);
+
+                expected.attach(key_chars.charAt(0), action, "test");
+
+                assertEquals(expected, actual);
+            } catch (SyntaxException err) {
+                fail(key_name + ": " + err);
+            }
+        }
+    }
+
+    @Test(timeout=5000)
+    public void testEscapes() {
+        String[][] escape_strings = {
+            {"\\b", "\b"},
+            {"\\f", "\f"},
+            {"\\n", "\n"},
+            {"\\r", "\r"},
+            {"\\t", "\t"},
+            {"\\x4e", "N"},
+        };
+
+        for (int i = 0; i < escape_strings.length; i++) {
+            String escape_text = escape_strings[i][0];
+            String escape_value = escape_strings[i][1];
+
+            Parser parser = parserFromString(
+                "k \"test\" `" + escape_text + "`\n");
+
+            try {
+                LeaderGroup actual = parser.parse();
+
+                LeaderGroup expected = new LeaderGroup();
+                LeaderAction action = new LeaderAction(escape_value, EMPTY_RUNNER);
+
+                expected.attach('k', action, "test");
+
+                assertEquals(expected, actual);
+            } catch (SyntaxException err) {
+                fail(escape_text + ": " + err);
+            }
+        }
+    }
 }
